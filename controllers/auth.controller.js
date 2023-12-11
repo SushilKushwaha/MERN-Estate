a33f0239aaadfda4492a46b46f23630c9f189af9
SushilKushwaha <sushilsinha733@gmail.com>
2023-12-11 19:23:23 +0545

    Complete sign in page functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,39 +1,51 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-
     const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password:hashedPassword });
     try {
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
-    res.status(201).json('User created successfully!');
-
+        res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
+        // Log the error or provide more specific details in the response
+        console.error('Error creating user:', error);
         next(error);
     }
-
 };
 
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(404, 'User not found!'));
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Wrong cridentials!'));
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-        const { password: pass, ...rest } = validUser._doc;
-        res
-            .cookie('access_token', token, { httpOnly: true})
-            .status(200)
-            .json(rest);
 
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found!'));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) {
+            return next(errorHandler(401, 'Wrong credentials!'));
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        // Omit sensitive information from the response
+        const { password: pass, ...rest } = validUser._doc;
+
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json({
+                user: rest,
+                authenticationStatus: 'authenticated',
+            });
     } catch (error) {
+        console.error('Error signing in:', error);
         next(error);
     }
-    
 };
